7c5d90e00a264c587624006975d0b434630e42e5
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-03-13 19:45:30 +0300

    Реализован алгоритм, исправлены баги

--- PchelkaMaya/PchelkaMaya/main.cpp
+++ PchelkaMaya/PchelkaMaya/main.cpp
@@ -4,15 +4,19 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <queue>
 
 using namespace std;
 
+const char FRIEND = 'D';
+const char MAYA = 'M';
+const char ENEMY = 'V';
+
 struct Node
 {
-	// 'D' - Friend
-	// 'M' - Maya
-	// 'V' - Enemy
 	char type;
+	int stepsCount = 0;
+	bool isChecked = false;
 
 	Node* topLeft = nullptr;
 	Node* topRight = nullptr;
@@ -25,11 +29,14 @@ struct Node
 int GetLevelsCount(ifstream& file);
 void CreateEmptyBeehive(vector<vector<Node>>& beehive, int levelsCount);
 void CreateLinksInBeehive(vector<vector<Node>>& beehive);
-void ReadBeehive(ifstream& file, vector<vector<Node>>& beehive, int levelsCount);
+void ReadBeehive(ifstream& file, vector<vector<Node>>& beehive);
+int GetBestStepsCount(vector<vector<Node>> const& beehiveToSearch);
+void MarkStepsFromNode(Node& node, int& finishStep);
+void CheckNode(queue<Node*>& nodesToCheck, Node* node, bool& isFinish, int& finishStep);
 
 // DEBUG
-void WriteBeehive(vector<vector<Node>> const& beehive);
-void WriteNode(Node const& node);
+void WriteBeehive(vector<vector<Node>> const& beehive, bool isWriteLinks);
+void WriteNode(Node const& node, bool isWriteLinks);
 
 
 int main(int argc, char* argv[])
@@ -44,10 +51,10 @@ int main(int argc, char* argv[])
 	vector<vector<Node>> beehive;
 	CreateEmptyBeehive(beehive, levelsCount);
 
-	WriteBeehive(beehive);
+	ReadBeehive(input, beehive);
+	CreateLinksInBeehive(beehive);
 
-	ReadBeehive(input, beehive, levelsCount);
-
+	output << GetBestStepsCount(beehive);
 
 	return 0;
 }
@@ -80,81 +87,218 @@ void CreateEmptyBeehive(vector<vector<Node>>& beehive, int levelsCount)
 
 		beehive.push_back(beehiveRow);
 	}
-
-	CreateLinksInBeehive(beehive);
 }
 
 void CreateLinksInBeehive(vector<vector<Node>>& beehive)
 {
 	size_t levelsCount = beehive.front().size();
+	size_t centerAdress = levelsCount - 1;
 
-	for (size_t i = 0; i < (levelsCount - 1); i++)
+	for (size_t i = 0; i < centerAdress; i++)
 	{
 		for (size_t j = 0; j < beehive[i].size(); j++)
 		{
 			beehive[i][j].bottomLeft = &beehive[i + 1][j];
 			beehive[i][j].bottomRight = &beehive[i + 1][j + 1];
 
+			beehive[i + 1][j].topRight = &beehive[i][j];
+			beehive[i + 1][j + 1].topLeft = &beehive[i][j];
+
 			if (j != 0)
 			{
-				beehive[i + 1][j].topLeft = &beehive[i][j];
+				beehive[i][j].left = &beehive[i][j - 1];
 			}
 			if (j != beehive[i].size() - 1)
 			{
-				beehive[i + 1][j].topRight = &beehive[i][j + 1];
+				beehive[i][j].right = &beehive[i][j + 1];
 			}
 		}
 	}
 
-	for (size_t i = levelsCount - 1; i < (2 * levelsCount - 2); i++)
+	for (size_t i = centerAdress; i < 2 * centerAdress; i++)
 	{
 		for (size_t j = 0; j < beehive[i].size(); j++)
 		{
 			if (j != 0)
+			{
 				beehive[i][j].bottomLeft = &beehive[i + 1][j - 1];
+				
+				beehive[i][j].left = &beehive[i][j - 1];
+			}
 			if (j != beehive[i].size() - 1)
+			{
 				beehive[i][j].bottomRight = &beehive[i + 1][j];
 
-			if (j != beehive[i + 1].size())
-			{
 				beehive[i + 1][j].topLeft = &beehive[i][j];
 				beehive[i + 1][j].topRight = &beehive[i][j + 1];
+
+				beehive[i][j].right = &beehive[i][j + 1];
 			}
+		}
+	}
+
+	for (size_t i = 1; i < levelsCount; i++)
+	{
+		beehive[2 * centerAdress][i].left = &beehive[2 * centerAdress][i - 1];
+	}
+	for (size_t i = 0; i < levelsCount - 1; i++)
+	{
+		beehive[2 * centerAdress][i].right = &beehive[2 * centerAdress][i + 1];
+	}
+}
+
+void ReadBeehive(ifstream& input, vector<vector<Node>>& beehive)
+{
+	string beehiveRow;
+	char type;
+
+	for (size_t i = 0; i < beehive.size(); i++)
+	{
+		getline(input, beehiveRow);
+		stringstream stream(beehiveRow);
+
+		for (size_t j = 0; j < beehive[i].size(); j++)
+		{
+			stream >> type;
+			beehive[i][j].type = type;
 		}
 	}
 }
 
-void ReadBeehive(ifstream& file, vector<vector<Node>>& beehive, int levelsCount)
+int GetBestStepsCount(vector<vector<Node>> const& beehiveToSearch)
 {
-	(void)file;
-	(void)levelsCount;
-	(void)beehive;
+	auto beehive = beehiveToSearch;
+	int finishStep = 0;
+
+	for (size_t i = 0; i < beehive.size(); i++)
+	{
+		for (size_t j = 0; j < beehive[i].size(); j++)
+		{
+			if ((i == 0) ||
+				(i == (beehive.size() - 1)) ||
+				(j == 0) ||
+				(j == beehive[i].size() - 1))
+			{
+				if (beehive[i][j].type != ENEMY)
+				{
+					MarkStepsFromNode(beehive[i][j], finishStep);
+				}
+			}
+		}
+	}
+
+	return finishStep;
+}
+
+void MarkStepsFromNode(Node& node, int& finishStep)
+{
+	node.stepsCount = 1;
+
+	queue<Node*> nodesToCheck;
+	nodesToCheck.push(&node);
+	bool isFinish = false;
+
+	while (!nodesToCheck.empty())
+	{
+		CheckNode(nodesToCheck, nodesToCheck.front(), isFinish, finishStep);
+		nodesToCheck.pop();
+	}
+}
+
+void CheckNode(queue<Node*>& nodesToCheck, Node* node, bool& isFinish, int& finishStep)
+{
+	int step = node->stepsCount;
+	node->isChecked = true;
+
+	if (node->type != ENEMY && !isFinish)
+	{
+		if (node->type != MAYA)
+		{
+			if (node->left && !node->left->isChecked)
+			{
+				node->left->stepsCount = step + 1;
+				nodesToCheck.push(node->left);
+			}
+			if (node->right && !node->right->isChecked)
+			{
+				node->right->stepsCount = step + 1;
+				nodesToCheck.push(node->right);
+			}
+			if (node->topLeft && !node->topLeft->isChecked)
+			{
+				node->topLeft->stepsCount = step + 1;
+				nodesToCheck.push(node->topLeft);
+			}
+			if (node->topRight && !node->topRight->isChecked)
+			{
+				node->topRight->stepsCount = step + 1;
+				nodesToCheck.push(node->topRight);
+			}
+			if (node->bottomLeft && !node->bottomLeft->isChecked)
+			{
+				node->bottomLeft->stepsCount = step + 1;
+				nodesToCheck.push(node->bottomLeft);
+			}
+			if (node->bottomRight && !node->bottomRight->isChecked)
+			{
+				node->bottomRight->stepsCount = step + 1;
+				nodesToCheck.push(node->bottomRight);
+			}
+		}
+		else
+		{
+			finishStep = node->stepsCount;
+			isFinish = true;
+		}
+	}
+}
+
+int GetStepsToMaya(vector<vector<Node>>& beehive)
+{
+	int steps = 0;
+
+	for (size_t i = 0; i < beehive.size(); i++)
+	{
+		for (size_t j = 0; j < beehive[i].size(); j++)
+		{
+			if (beehive[i][j].type == MAYA)
+			{
+				steps = beehive[i][j].stepsCount;
+				break;
+			}
+		}
+	}
+
+	return steps;
 }
 
 // DEBUG
 
-void WriteBeehive(vector<vector<Node>> const& beehive)
+void WriteBeehive(vector<vector<Node>> const& beehive, bool isWriteLinks)
 {
 	for (size_t i = 0; i < beehive.size(); i++)
 	{
 		for (size_t j = 0; j < beehive[i].size(); j++)
 		{
-			WriteNode(beehive[i][j]);
+			WriteNode(beehive[i][j], isWriteLinks);
 		}
 		cout << endl;
 	}
 }
-
-void WriteNode(Node const& node)
+void WriteNode(Node const& node, bool isWriteLinks)
 {
 	cout << '[';
 
-	if (node.left)  cout << "|L|";
-	if (node.topLeft)  cout << "|TL|";
-	if (node.topRight)  cout << "|TR|";
-	if (node.right)  cout << "|R|";
-	if (node.bottomRight)  cout << "|BR|";
-	if (node.bottomLeft)  cout << "|BL|";
+	cout << node.type << ',';
 
+	if (isWriteLinks)
+	{
+		if (node.left)  cout << "|L|";
+		if (node.topLeft)  cout << "|TL|";
+		if (node.topRight)  cout << "|TR|";
+		if (node.right)  cout << "|R|";
+		if (node.bottomRight)  cout << "|BR|";
+		if (node.bottomLeft)  cout << "|BL|";
+	}
 	cout << ']';
 }
