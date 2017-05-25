8bd1d023b2fe9facbc01118e6747908b0264b76f
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-05-25 19:51:50 +0300

    Решено задание Газон

--- IvanLawn/IvanLawn/main.cpp
+++ IvanLawn/IvanLawn/main.cpp
@@ -4,32 +4,33 @@
 #include <cmath>
 
 using namespace std;
+typedef long long Int;
 
 struct Circle
 {
-	int x;
-	int y;
-	int radius;
+	Int x;
+	Int y;
+	Int radius;
 };
 struct Rect
 {
-	int x1;
-	int y1;
-	int x2;
-	int y2;
+	Int x1;
+	Int y1;
+	Int x2;
+	Int y2;
 
-	size_t LineIntersectsCount(int lineX, int lineMinY, int lineMaxY)
+	Int LineIntersectsCount(Int lineX, Int lineMinY, Int lineMaxY)
 	{
 		if (lineX < x1 || lineX > x2 || lineMinY > y2 || lineMaxY < y1)
 		{
 			return 0;
 		}
 
-		return size_t(min(lineMaxY, y2) - max(lineMinY, y1) + 1);
+		return min(lineMaxY, y2) - max(lineMinY, y1) + 1;
 	}
 };
 
-size_t Calculate(Rect rect, Circle circle);
+Int Calculate(Rect rect, Circle circle);
 void ReadInput(std::ifstream &input, Rect &rect, Circle &circle);
 
 int main()
@@ -41,32 +42,32 @@ int main()
 	Circle circle;
 	ReadInput(input, rect, circle);
 
-	size_t result = Calculate(rect, circle);
+	Int result = Calculate(rect, circle);
 	output << result;
 
 	return 0;
 }
 
-size_t Calculate(Rect rect, Circle circle)
+Int Calculate(Rect rect, Circle circle)
 {
-	int centerMinY = circle.y - circle.radius;
-	int centerMaxY = circle.y + circle.radius;
-	size_t result = rect.LineIntersectsCount(circle.x, centerMinY, centerMaxY);
-	int currentY = circle.radius;
+	Int centerMinY = circle.y - circle.radius;
+	Int centerMaxY = circle.y + circle.radius;
+	Int result = rect.LineIntersectsCount(circle.x, centerMinY, centerMaxY);
+	Int currentY = circle.radius;
 
-	for (int currentX = 1; currentX <= circle.radius; currentX++)
+	for (Int currentX = 1; currentX <= circle.radius; currentX++)
 	{
 		while (pow(currentX, 2) + pow(currentY, 2) > pow(circle.radius, 2))
 		{
 			currentY--;
 		}
 
-		int minY = circle.y - currentY;
-		int maxY = circle.y + currentY;
-		int leftX = circle.x - currentX;
-		int rightX = circle.x + currentX;
-		size_t leftCount = rect.LineIntersectsCount(rightX, minY, maxY);
-		size_t rightCount = rect.LineIntersectsCount(leftX, minY, maxY);
+		Int minY = circle.y - currentY;
+		Int maxY = circle.y + currentY;
+		Int leftX = circle.x - currentX;
+		Int rightX = circle.x + currentX;
+		Int leftCount = rect.LineIntersectsCount(rightX, minY, maxY);
+		Int rightCount = rect.LineIntersectsCount(leftX, minY, maxY);
 		result += (leftCount + rightCount);
 	}
 
